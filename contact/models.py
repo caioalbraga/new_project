f95046c2047154ab56718daa12b964c4fa1ed6b0
caioalbraga <kaioabraga@gmail.com>
2025-01-28 17:27:09 -0300

    Criando Model category e fazendo ligações com FK

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-
-# Create your models here.
 from django.utils import timezone
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Contact(models.Model):
@@ -20,8 +25,12 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')  
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-       
