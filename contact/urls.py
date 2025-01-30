0a7cc8f0bb67e7d4883466c44ed04242621c90dd
caioalbraga <kaioabraga@gmail.com>
2025-01-30 13:24:26 -0300

    Criando URL view e template para criar um contato

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,4 @@
+# type:ignore
 from django.urls import path
 
 from contact import views
@@ -7,6 +8,8 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+
     # contact (CRUD)
     path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
