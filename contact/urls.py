9bcb2aeeb3ac51f979cde7208b46453137304115
caioalbraga <kaioabraga@gmail.com>
2025-01-30 13:07:10 -0300

    .

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,8 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
